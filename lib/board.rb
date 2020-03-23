f8554104eb6d6d0640be48aeaefcbc26764c3d0c
rsnazario <rafael.s.nazario@gmail.com>
2020-03-23 12:04:54 -0300

    Refactored Code

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,25 @@
 class Board
-  def initialize; end
-
-  # method returns different boards available for game play
-  def game_board(which_board)
-    if which_board == 1
-      game_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    elsif which_board == 2
-      instr_board = ['X', ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ']
-    elsif which_board == 3
-      logo_board = %w[0 1 2 3 4 5 6 7 8]
-    else
-      p 'Error'
-    end
+  attr_accessor :board
+  @@logo_board = %w[1 2 3 4 5 6 7 8 9]
+  @@instr_board = ['X', ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ']
+  
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 
-  # Method used for printing dummy boards
-  def print(board)
-    puts " #{board[0]}|#{board[1]}|#{board[2]}".center(38)
-    puts " #{board[3]}|#{board[4]}|#{board[5]}".center(38)
-    puts " #{board[6]}|#{board[7]}|#{board[8]}".center(38)
+  def clear_board
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  end
+
+  def self.logo
+    @@logo_board
+  end
+
+  def self.instr
+    @@instr_board
+  end
+
+  def self.make_move(table, move, mark)
+    table[move.to_i - 1] = mark
   end
 end
