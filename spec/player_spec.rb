831519556e96b75715b73296277d0a588919af0a
rsnazario <rafael.s.nazario@gmail.com>
2020-03-24 18:29:26 -0300

    Rspec for Player Class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,5 +2,11 @@
 require './lib/player.rb'
 
 RSpec.describe Player do 
-  
+  let(:count) {0}
+  describe '#update_wins' do 
+    context 'Once it is called, increment count by one' do
+      p = Player.new('Player_One')
+      it {expect(p.update_wins).to eq(p.count)}
+    end
+  end
 end
