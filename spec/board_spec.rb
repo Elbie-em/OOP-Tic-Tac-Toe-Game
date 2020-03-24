26b6d6ed547138b59914188467c223873cb366d9
rsnazario <rafael.s.nazario@gmail.com>
2020-03-24 19:43:46 -0300

    Rspec Tests for Game Class and Player Class

--- spec/board_spec.rb
+++
@@ -1,6 +0,0 @@
-#spec :game_spec.rb
-require './lib/board.rb'
-
-RSpec.describe Board do 
-  
-end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,12 @@
-#spec :game_spec.rb
+# spec :game_spec.rb
 require './lib/player.rb'
 
-RSpec.describe Player do 
-  let(:count) {0}
-  describe '#update_wins' do 
+RSpec.describe Player do
+  let(:count) { 0 }
+  describe '#update_wins' do
     context 'Once it is called, increment count by one' do
       p = Player.new('Player_One')
-      it {expect(p.update_wins).to eq(p.count)}
+      it { expect(p.update_wins).to eq(p.count) }
     end
   end
 end
